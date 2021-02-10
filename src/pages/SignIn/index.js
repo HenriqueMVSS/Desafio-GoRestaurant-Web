4cc4dd50c5cad5433383694f377d171a7d44acdf
Henrique Mateus Vieira dos S. Silva <henrique.silva@fbest.org.br>
2021-02-10 11:19:19 -0300

    Desafio: GoRestaurant Web

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+import { FaSpinner } from 'react-icons/fa';
+
+import { SubmitButton } from '~/pages/_layouts/auth/styles';
+
+import { signInRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/images/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O email é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
+
+export default function SignIn() {
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu email" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+        <SubmitButton loading={loading ? 1 : 0}>
+          {loading ? <FaSpinner size={16} color="#fff" /> : 'Acessar'}
+        </SubmitButton>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
+}
